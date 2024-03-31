89b20f2608c30c60619f2766e0c5bb84537e6def
ValentinUnileon <74121671+ValentinUnileon@users.noreply.github.com>
2024-04-01 00:27:35 +0200

    implementing method updatePuzzle

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ def main():
 
     print("\n")
 
+    """""
     #PROBATINAS
         
     matrizSoluciones = getSolution(puzzle_lines)
@@ -45,22 +46,26 @@ def main():
     print("El puzle esta resuelto: ",isSolved(puzzle_lines, matrizSoluciones))
     x, y = getCharPositionXY(puzzle_lines)
     print("la posicion incial es", x, " ", y)
+    print ("la anchura en ese punto es", len(puzzle_lines[y+1]))
 
     #print("El siguiente movimiento es valido: ", isMovementValid(x, y, puzzle_lines))
     
-    print("Tiene dos paredes la caja cuando se mueva? o se va a mover a una pared la caja", isBoxWithTwoWallsANDNotMovingToWall(x+1, y, puzzle_lines))
+    print("Tiene dos paredes la caja cuando se mueva? o se va a mover a una pared la caja", isBoxWithTwoWallsANDNotMovingToWall(x, y-1, puzzle_lines))
+    print("Es un movimiento invalido al moverse a un pared?", isNearWallWithOutGoal(x+1, y, puzzle_lines))
 
     #FIN PROBATINAS
+
+    """
 
     #Llamamos al metodo para resolver el tablero
         #falta metodo para obtener la posicion inicial del personaje
         #falta metodo para obtener la solucion final
-    #steps = []
-    #if solve_Sokoban_backtracking():
-    #    print("Se ha encontrado solucion")
+    steps = []
+    if solve_Sokoban_backtracking():
+        print("SE ha encontrado solucion")
         #imprimir steps (los devuelve el metodo de backtracking)
-    #else:
-    #    print("nose")
+    else:
+        print("NO se ha encontrado la solucion")
     
 
 
@@ -78,7 +83,7 @@ def solve_Sokoban_backtracking(x, y, puzzle, steps, solution):
         
         #Ahora hay que plantear todos los movimentos posibles del personaje
         #Arriba - abajo - derecha - izquierda
-        if solve_Sokoban_backtracking(x+1, y, updatePuzzle(x, y, puzzle), steps):
+        if solve_Sokoban_backtracking(x+1, y, updatePuzzle(x, y, x+1, y, puzzle), steps):
             return True, steps + ["right"]
         
         if solve_Sokoban_backtracking(x-1, y, updatePuzzle(x, y, puzzle), steps):
@@ -145,7 +150,7 @@ def isMovementValid(x, y, puzzle):
     #Para empezar tenemos que tener en cuenta que la posicion actual se encuentra en puzzle y es valida seguro
     #La nueva tenemos que construirla con x e y 
 
-    xP, yP = getCharPositionXY(puzzle)
+    #xP, yP = getCharPositionXY(puzzle)
     if puzzle[y][x] == "#":
         return False
     
@@ -154,23 +159,86 @@ def isMovementValid(x, y, puzzle):
     if isBoxWithTwoWallsANDNotMovingToWall(x, y, puzzle) : #tambien se comprueba si se va a mover hacia una pared Y si no va a mover una caja da FALSE
         return False  #NO es valido                        #*****hay que comprobar cuando la caja ya esta pegada a la pared
     
-  # if isNearWallWithOutGoal():
+    if isNearWallWithOutGoal(x, y, puzzle):
+        return False #NO es valido
+
+  # if isNearWallWithOutGoal(): #metodo que, si la caja se mueve hacia un pared, comprueba si tiene en la vertical o en la horizontal un objetivo, en ese caso el mov sera valido, sino no
         #return False
-  # if la caja esta pegada contra la pared y quieres empujarla
+  # if la caja esta pegada contra la pared y quieres empujarla -- ya esta
         #return False
-  # if va a empujar a una caja y tiene otra en frente
+  # infrontOfOtherBox() if va a empujar a una caja y tiene otra en frente
         #return False
 
 
     return True
-    
+
+
+def isNearWallWithOutGoal(x, y, puzzle): #no hace falta comprobar si se va a mover hacia una pared
+
+    if puzzle[y][x] == "$": #Entonces va a mover una caja
+
+        if puzzle[y + 1][x] == "@" and puzzle[y - 1][x] != ".":
+
+            if puzzle[y-2][x] in {"#", "!"} and puzzle[y-2][x+1] in {"#", "!"} and puzzle[y-2][x-1] in {"#", "!"}:
+                #si tiene 3 cajas alrededor hay que comprobar si hay un . en su vertical porque si no es un movimiento invalido
+                #ahora hay que comprobar si la horizontal tiene algun .
+                for row in puzzle[y-1]:
+                    if row == ".":
+                        return False #Puede moverse a esa posicion
+
+                return True   
+            
+        if puzzle[y - 1][x] == "@" and puzzle[y + 1][x] != ".":
+            
+            if puzzle[y+2][x] in {"#", "!"} and puzzle[y+2][x+1] in {"#", "!"} and puzzle[y+2][x-1] in {"#", "!"}:
+
+                for row in puzzle[y+1]:
+                    if row == ".":
+                        return False # Puede moverse a esa posicion
+                    
+                return True
+            
+        if puzzle[y][x+1] == "@" and puzzle[y][x-1] != ".":
+            
+            if puzzle[y][x-2] in {"#", "!"} and puzzle[y+1][x-2] in {"#", "!"} and puzzle[y-1][x-2] in {"#", "!"}:
+
+                for i in range(len(puzzle)):
+                    cell = puzzle[i][x-1]
+                    if cell == ".":
+                        return False  # Puede moverse a esa posición
+
+                return True
+            
+        if puzzle[y][x-1] == "@" and puzzle[y][x+1] != ".":
+
+            if puzzle[y][x+2] in {"#", "!"} and puzzle[y+1][x+2] in {"#", "!"} and puzzle[y-1][x+2] in {"#", "!"}:  #falta implementar fullLineWallX()
+
+                for i in range(len(puzzle)):
+                    cell = puzzle[i][x+1]
+                    if cell == ".":
+                        return False  # Puede moverse a esa posición
+
+                return True                
+
+
+    return False
+
+
+
+def fullLineWallX(x, y, puzzle):
+    #metodo utilizado para saber si la pared a la que se va a acercar la caja esta llena de paredes por debajo y por arriba, en tal caso el mov es invalido
+    for row in puzzle[y+2]:
+        if row != "#":
+            return False #No esta full de paredes por lo tanto el mov es valido
+
+def fullLineWallY(x, y, puzzle):
+    print("nose")
 
 def isBoxWithTwoWallsANDNotMovingToWall(x, y, puzzle):
-    counter = 0
 
     # Detectamos si el personaje está al lado de una caja
     if puzzle[y][x] == "$":  # Entonces va a mover una caja
-
+        counter = 0
         # Buscamos hacia dónde va a mover la caja
 
         if puzzle[y + 1][x] == "@" and puzzle[y - 1][x] != ".":
@@ -231,9 +299,21 @@ def isBoxWithTwoWallsANDNotMovingToWall(x, y, puzzle):
 
 
 
-def updatePuzzle(x, y, puzzle):  #cuando una caja se pone encima de un objetivo se tiene que cambiar al caracter !
-    print("nose")
-    #devuelve puzle con las posiciones movidas
+def updatePuzzle(xP, yP, x, y, puzzle):  # Cuando una caja se pone encima de un objetivo se tiene que cambiar al caracter '!'
+
+    if puzzle[y][x] == "$":  # Entonces va a mover una caja
+        # Código correspondiente a este caso
+        print("hola")
+    elif puzzle[y][x] == ".":  # Si el personaje estaba encima de un objetivo debería de cambiar de forma
+        print("nola")
+    elif puzzle[yP][xP] == "&":
+        
+
+    return False
+
+
+
+   
 
 
 
